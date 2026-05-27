15b7a94986c24a94026a3fabaa14d4668a4fe224
christian <christian.garcia@dgcp.gob.sv>
2026-05-27 08:39:37 -0600

    Agregado Diccionario de Productos, desactivacion de registros en catalogos

---
+++ inventario-frontend/src/app/components/producto/producto-diccionario/producto-diccionario.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-diccionario',
+  imports: [],
+  templateUrl: './producto-diccionario.component.html',
+  styleUrl: './producto-diccionario.component.css',
+})
+export class ProductoDiccionarioComponent {
+
+}
